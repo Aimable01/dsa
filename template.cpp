261a921dd6a0e06a085807e00663b7c7831cf40d
Aimable01 <aimable.kwizera14@gmail.com>
2024-10-30 13:29:23 +0200

    fix usage of template

--- template.cpp
+++ template.cpp
@@ -6,8 +6,16 @@ T addition(T a,T b){
     return a + b;
 }
 
+template<typename K, typename T>
+K addition(K a,T b){
+    return a + b;
+}
+
 int main(){
-    cout << "Addition of 2 numbers" << addition(10, 20) << endl;
+    cout << "Addition of 2 integer numbers is: " << addition<int>(10, 20) << endl;
+    cout << "Addition of 2 double numbers is: " << addition<double>(10.32, 20.43) << endl;
+    cout << "Addition of 2 short numbers is: " << addition<short>(6, 8) << endl;
+    cout << "The addition of a double and an integer is: " << addition<double,int>(10.32, 65) << endl;
 
     return 0;
 }
