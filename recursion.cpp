4c7c98d76aea29fb39b30307156127008ae6415a
Aimable01 <aimable.kwizera14@gmail.com>
2024-10-31 15:52:31 +0200

    feat: add fibbonacci

--- recursion.cpp
+++ recursion.cpp
@@ -3,10 +3,13 @@ using namespace std;
 
 // the factorial of a number
 int fact(int n){
+
+    // base case
     if(n == 0 || n == 1){
         return 1;
     }
 
+    // recursive case
     int smallAnswer = fact(n - 1);
 
     return n * smallAnswer;
@@ -14,13 +17,48 @@ int fact(int n){
 
 // the sum of digits of a number
 int sumDigits(int n){
+
+    // base case
     if(n < 10){
         return n;
     }
 
+    // recursive case
     int smallAnswer = sumDigits(n / 10);
 
     return n % 10 + smallAnswer;
+}
+
+// the power of a number
+int power(int x, int n) {
+
+    // base case
+    if(n == 0){
+        return 1;
+    }
+
+    // recursive case
+    int smallAnswer = power(x, n - 1);
+
+    return x * smallAnswer;
+}
+
+// fibonnacci
+int fibbonacci(int n){
+
+    // base case
+    if(n == 0){
+        return 0;
+    }
+    if(n == 1){
+        return 1;
+    }
+
+    // recursive case
+    int smallOutput1 = fibbonacci(n - 1);
+    int smallOutput2 = fibbonacci(n - 2);
+
+    return smallOutput1 + smallOutput2;
 }
 
 int main(){
@@ -32,4 +70,13 @@ int main(){
     int sum = sumDigits(num);
     cout << "The sum of digits of " << num << " is: " << sum << endl;
 
+    int numBase = 2, numPower = 5;
+    int powerAnswer = power(numBase, numPower);
+    cout << "The power of " << numBase << " is: " << powerAnswer << endl;
+
+    int fibNum = 6;
+    int fibAnswer = fibbonacci(fibNum);
+    cout << "The fibbonacci number at " << fibNum << " is: " << fibAnswer << endl;
+
     return 0;
+}
