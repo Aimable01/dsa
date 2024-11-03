f2e3730d327ae11a93dd54f270e79bd440ff751a
Aimable01 <aimable.kwizera14@gmail.com>
2024-11-03 07:54:14 +0200

    feat: interest calculator

--- rev.cpp
+++ rev.cpp
@@ -12,7 +12,27 @@ int main(){
     getline(cin, name);
     cout << "Enter your age: ";
     cin >> age;
+    cin.ignore();
     cout << "Your name is: " << name << " and you are: " << age << endl;
+
+    // interest calculator
+    string fullname;
+    double initialAmount;
+    float interestRate;
+    int years;
+    double totalAmount;
+
+    cout << "Enter your fullname: ";
+    getline(cin, fullname);
+    cout << "Enter the initial amount: ";
+    cin >> initialAmount;
+    cout << "Enter the interest rate (eg: 2.34): ";
+    cin >> interestRate;
+    cout << "Enter the number of years: ";
+    cin >> years;
+
+    totalAmount = initialAmount * interestRate * years;
+    cout << fullname << " ,the total amount is: " << totalAmount << endl;
 
     return 0;
 }
