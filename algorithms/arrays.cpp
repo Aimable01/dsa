7cddc6c5e4be5213fc7c82e95fc26e59514eac5e
Aimable01 <aimable.kwizera14@gmail.com>
2024-11-13 10:55:00 +0200

    feat: 2d arrays

--- algorithms/arrays.cpp
+++ algorithms/arrays.cpp
@@ -3,10 +3,11 @@ using namespace std;
 
 int main(){
 
-    int marks[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int marks[9] = {52, 78, 75, 68, 88, 63, 75, 90, 78};
     for (int i = 0; i < 9; i++){
-        cout << marks[i] << " ";
+        cout << marks[i] << ", ";
     }
+    cout << endl;
 
     return 0;
 }
