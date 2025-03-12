0500934b6004b95525400ee2ef51124a9deffc34
Aimable01 <aimable.kwizera14@gmail.com>
2025-03-12 11:29:09 +0200

    feat: non-linear data structures: graphs

---
+++ non-linear data structures/graph_adjacency_matrix.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class AdjacencyMatrix
+{
+private:
+    int n;
+    int **adj;
+
+public:
+    AdjacencyMatrix(int n)
+    {
+        /**
+         * Initializing the adjacency matrix
+         */
+    }
+};
