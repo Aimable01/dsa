f45af4699857b542f8e35b1868a21b2445c6af49
Aimable01 <aimable.kwizera14@gmail.com>
2025-03-19 10:51:20 +0200

    fix: also print in reverse order

--- non-linear data structures/dfs_connected_graph.cpp
+++ non-linear data structures/dfs_connected_graph.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
 void printDFS(vector<vector<int>> v, int sv, vector<bool> &visited)
 {
     // SV as a Starting vertex
-    cout << sv << endl;
+    cout << sv << " ";
     visited[sv] = true;
     int n = v.size();
+
+    // This one prints in forward order
     for (int i = 0; i < n; i++)
     {
         if (v[sv][i] == 1 && visited[i] == false)
@@ -14,15 +17,28 @@ void printDFS(vector<vector<int>> v, int sv, vector<bool> &visited)
             printDFS(v, i, visited);
         }
     }
+
+    /*
+    // This one prints in reverse order
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (v[sv][i] == 1 && visited[i] == false)
+        {
+            printDFS(v, i, visited);
+        }
+    }
+    */
 }
 int main()
 {
     int n, e;
     cout << "Enter the number of vertices and edges" << endl;
     cin >> n >> e;
+
     vector<vector<int>> matrix(n, vector<int>(n, 0));
     cout << "Reading" << n << "vertices numbered from 0 to " << n - 1 << endl;
     cout << "Enter first Vertex and second vertex" << endl;
+
     for (int i = 1; i <= e; i++)
     {
         int fv, sv;
@@ -30,8 +46,9 @@ int main()
         matrix[fv][sv] = 1;
         matrix[sv][fv] = 1;
     }
-    cout << "DFS " << endl;
+    cout << "DFS: ";
     vector<bool> visited(n, false);
     printDFS(matrix, 0, visited);
+
     return 0;
 }
