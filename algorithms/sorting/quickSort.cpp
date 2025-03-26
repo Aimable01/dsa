20e3e5d1a11136bd54131369ce4d93fd56ed14d4
Aimable01 <aimable.kwizera14@gmail.com>
2025-03-26 20:41:14 +0200

    feat: quick sort algorithm

--- algorithms/sorting/quickSort.cpp
+++ algorithms/sorting/quickSort.cpp
@@ -1,7 +1,45 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int partition(int arr[], int s, int e)
+{
+    int pivot = arr[e];
+    int i = s - 1;
 
+    for (int j = s; j < e; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[e]);
+    return i + 1;
+}
+
+void quickSort(int arr[], int s, int e)
+{
+    if (s >= e)
+    {
+        return;
+    }
+
+    int p = partition(arr, s, e);
+    quickSort(arr, s, p - 1);
+    quickSort(arr, p + 1, e);
+}
+
+int main()
+{
+    int arr[5] = {23, 5, 11, 7, 13};
+
+    quickSort(arr, 0, 4);
+
+    // Output the sorted array
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }
     return 0;
 }
