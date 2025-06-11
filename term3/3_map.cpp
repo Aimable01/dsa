41888cb72beabf18001aeb1bcb6e33a9078366ec
Aimable01 <aimable.kwizera14@gmail.com>
2025-06-11 10:57:29 +0200

    feat: ordered map

---
+++ term3/3_map.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main()
+{
+    // creating a map
+    map<string, int> myStock;
+
+    // inserting a new key-value pair
+    myStock["apple"] = 100;
+    myStock["banana"] = 200;
+    myStock["cherry"] = 300;
+    myStock["pineapple"] = 500;
+    myStock["mango"] = 600;
+
+    // retrieving the value associated with a key
+    int qty = myStock["banana"];
+    cout << "Value for key 'banana': " << qty << endl;
+
+    // updating the value associated with a key
+    myStock["banana"] = 250;
+    qty = myStock["banana"];
+    cout << "updated value for key 'banana': " << qty << endl;
+
+    // removing a key-value pair
+    cout << "Key-value pairs in the map: " << endl;
+    for (const auto &pair : myStock)
+    {
+        cout << pair.first << " , " << pair.second << endl;
+    }
+
+    return 0;
+}
